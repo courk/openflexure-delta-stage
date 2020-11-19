54d7464fcadbbeaeb7651f12c259c19bc204c2ab
Samuel McDermott <sjm263@cam.ac.uk>
2020-11-19 10:50:36 +0000

    Add build for petri dish holder
    Former-commit-id: 0ae3c2c885549c2c7b379b44bc5dd910ea7da499

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ openscad -o builds/base_raspi_sangaboard.stl openscad/base_raspi_sangaboard.scad
 openscad -o builds/base.stl openscad/base.scad
 openscad -o builds/feet.stl openscad/feet.scad
 
+#optional parts
+openscad -o builds/35mm_petri_dish_holder.stl openscad/35mm_petri_dish_holder.scad
+
 # parts from the OFM repository
 openscad -o builds/actuator_assembly_tools.stl openflexure-microscope/openscad/actuator_assembly_tools.scad
 openscad -o builds/sample_clips.stl openflexure-microscope/openscad/sample_clips.scad
