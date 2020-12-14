2f392ba077a1518d557f8573be7f882d291b58cb
Samuel McDermott <sjm263@cam.ac.uk>
2020-12-14 14:48:20 +0000

    update build script for new OFM parts
    Former-commit-id: ae3a1af46550ab467d40a7773f5ac1a1f2cfa4a0 [formerly 9f6883e50fcaae3b9d62c19161038c2f7068b341] [formerly 65f4be11d97f55a48cbf0d6108bc5d6bfb099c56 [formerly e7a40f9346d1e57e344555d2cec7800df5fb6d78]] [formerly d2ac8f4b2dc83614e8cae04cb042d93f491f1542 [formerly b88afd62e2c5e58c368837e3990526df8a4c5bbf] [formerly ee8f2aff791d9824d0c972c9701622f3919ae9c9 [formerly 3cf611f36c8afb634d764c609b9241e2874b6890]]] [formerly 40e2c6a7af50a42e180c4531c3ba04673a8e11fa [formerly e90f0fc8f195087b45da1b2b1641255928c7b0ef] [formerly 3c6445521c27825f5a400a25c1678ba80b960c9a [formerly a2a365f08fa0175f9a2f3901e269266ccc5b3676]] [formerly fc63216643afa89effbf254a45426d1ff03d3584 [formerly 7aedaf429d092c3e5e256d3e565e599fd949c959] [formerly 57c4f5f5f7230af94defbc82141df33cda1b9caa [formerly 468f92cf0050be4411f88ca1d74ffd25c71b643a]]]] [formerly b57c7969412854fe483f91130a3e4852567db76c [formerly 95a4e8523e660d44168eb92c9c7bedd9e765269d] [formerly 36c528206792d3c629dc0eceaac6a606d9ad3f9e [formerly c1e9e809aa6a81398f8c1009a6fd9f36f4d953ed]] [formerly fd88e7f81269d346596350c109d027724690db1b [formerly 29e2ba98e8e0d9dd5ad3a3ce6d279c3890dd5b20] [formerly 0021c659bd1a19d2a712d39867b3cd7e93dcd6d8 [formerly c4b063a5c3a1f35080b803eee606603dbc51c300]]] [formerly 1728a809bfcd6aa45f0623ed131e77e10faf3205 [formerly 734d1d71584a12132c8f6ef218185e47b0e77ff9] [formerly bbe94e5722bac80c4913ae787e952820cc3b0dd8 [formerly 3b8858838e4662b504b7d80fd4aaff9fc8db65c6]] [formerly 435430de183ff6ec6c61ad9559148ab88aa53ace [formerly 1224046e3c0668a07126b31adc51319c4d15524c] [formerly 44d3e2a91a8cb23c55970a4ea647dd995f404675 [formerly 79db530a99d5e6ee50349235f67c01786114c0d2]]]]]
    Former-commit-id: a0fabb98471f8f4bba327ff516d3e59328cb9882 [formerly 8afcd57616554842be32c6f9dfda1f85d81aad06] [formerly dd40824c078ca896640bfb4d2fd1ca4d570835a9 [formerly f58b244ff2b7d6baa5fadcb2dfdbab0b2d7a64f3]] [formerly a577b0f665caa74df43253d64da55104d05527fc [formerly bac0c975ad5a63ceb0667433ca07fc231eb6bb6e] [formerly 050ac498e974bb24433c5105dc12c84f41d1cc72 [formerly d03ad821057d56bbc560081171079aa70abd0613]]] [formerly 00a0dbadef0058bed7c179067eeb1e6afe59acce [formerly f412ea6debbe23bbeecc8ca2d6c118d366832455] [formerly 59aead3ff6decec31199664eae0f9a3b7e5dd9d5 [formerly cea57028f74af0930511719211c6995ab02ff0e5]] [formerly 8079861b1e9fe086da587d0cbfd103dbb12fb788 [formerly aa67cfc2b01bd850e3183b0789a58b2012f61ddd] [formerly 18d188662c230f72b51cfe00c0d000ed133f9f6c [formerly 495cc675831c89455e8e449d422f9f0f99802c13]]]]
    Former-commit-id: bc16346ab27f1ca496ef191c8a489096bf87b16d [formerly 604cde336530b5c89a796da7d9bf51ad09db19f5] [formerly 78ea4d4e14330ee0e476d882265ed5ac47d9c5ec [formerly 70a13a891d1c8238209f1db7adfe163b90bae4ad]] [formerly 487c8c375359eb2318526baab4f9038977881f8a [formerly 84ad14b4de5de3f35a768109fe167cf59d76e4e8] [formerly 323806462462f9a0243be4a9a480a3cec46985ba [formerly 1d67994c04564e24557bdda6d512fd6bca218595]]]
    Former-commit-id: 3a54c0297fef1e4f30fd6f7989908f77aa8ff1c3 [formerly 6f1932ce27fd5dd92aad98a50b61da477d53d775] [formerly b5d6ed2dd1facece05b542637d429e252fc247a3 [formerly 52ee748eacf484c497ec46980ec9a02bc7b186e6]]
    Former-commit-id: a6fb85629f6cca41d0cc527cbcd77ac57341448b [formerly 54fd2a7cee89b121bb73c0301e726b4402e5fddd]
    Former-commit-id: c696e5c2269cbe5134b6180e501e89f38925c40b

--- build.sh
+++ build.sh
@@ -13,18 +13,30 @@ openscad -o builds/feet.stl openscad/feet.scad
 
 #optional parts
 openscad -o builds/35mm_petri_dish_holder.stl openscad/35mm_petri_dish_holder.scad
-#TODO
-#LED grid holder
 
-# parts from the OFM repository
+# TODO:
+# New parts from OFM repository
+# This is temporary.  Once these parts have been released on a tag on the OFM repository,
+# the delta stage has been updated to work with the new OFM repository,
+# the submodule should be updated to that tag and the parts can be built without checking out to this commit.
+
+cd openflexure-microscope/
+git checkout 4dfc357ee2cffe9bf95603c26d42fa8b9b22fec6
+cd ../
+
 openscad -o builds/actuator_assembly_tools.stl openflexure-microscope/openscad/actuator_assembly_tools.scad
 openscad -o builds/sample_clips.stl openflexure-microscope/openscad/sample_clips.scad
 openscad -o builds/small_gears.stl openflexure-microscope/openscad/small_gears.scad
-openscad -o builds/gears.stl openflexure-microscope/openscad/gears.scad
+openscad -o builds/large_gears.stl openflexure-microscope/openscad/large_gears.scad
 openscad -o builds/illumination_dovetail.stl openflexure-microscope/openscad/illumination_dovetail.scad
-# TODO:
-# condenser holder
-# Optics modules: Delta stage transmission and reflection
-# camera cover
-# filter cube
-# reflection illumination parts
+openscad -o builds/picamera_2_cover.stl openflexure-microscope/openscad/cameras/picamera_2_cover.scad
+openscad -o builds/fl_cube.stl openflexure-microscope/openscad/fl_cube.scad
+openscad -o builds/condenser.stl openflexure-microscope/openscad/condenser.scad
+openscad -o builds/optics_picamera2_rms_f50d13_delta.stl -D 'optics = "rms_f50d13"' -D 'camera = picamera_2' -D 'delta_stage = true' openflexure-microscope/openscad/optics.scad
+openscad -o builds/optics_picamera2_rms_f50d13_beamsplitter_delta.stl -D 'optics = "rms_f50d13"' -D 'camera = picamera_2' -D 'delta_stage = true' -D 'beamsplitter = true' openflexure-microscope/openscad/optics.scad
+openscad -o builds/LED_array_holder.stl openflexure-microscope/openscad/LED_array_holder.scad
+openscad -o builds/reflection_illuminator_LED_star.stl openflexure-microscope/openscad/reflection_illuminator_LED_star.scad
+
+cd openflexure-microscope/
+git checkout d4b73bbb5de523235a97d4277fd1d197f9b1997f
+cd ../
