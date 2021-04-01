e4452d6c4e128370a38f1b3a633b5c1726a5159d
samuelmcdermott <sjm263@cam.ac.uk>
2021-04-01 19:29:22 +0100

    Update submodule to OFM v6.1.2
    Former-commit-id: 157d35a3872e2fbd801b501acb22f9feb14fa589 [formerly 44149fbbad1cc7d1dc3456ad77504c6436b6f8fc] [formerly 1586ba00f3b035399fd56f439c9f6c91f5517150 [formerly 4a821d3ed6e1b014d6770163e7a28fa53995e9f6]] [formerly 41eceec5ba1b108ed5d4ace94dd747275af80ebc [formerly d06692dee6af345aa2b02f87376c251e6bc7423f] [formerly 5bee6b7fdee567a3270f668f2d911557240e0658 [formerly 3c9afd89b3d77794f80aa64d9e998f45fcf3d261]]] [formerly 6a5963924c7cd13b51588cafdbbad038eec3437b [formerly aff6f21d826f55174e7eaeb3554d271c87e26058] [formerly 2e1b33ae2dcf70a3dc8fcf12172c33ce8f6e7031 [formerly 84b257163f2f87fb1ebd6f91d68c92e8af298e55]] [formerly cde2a96487671671b0fffc241dc612f4da926306 [formerly 87c815b48d636b77baba16e2c40f5b279de17c55] [formerly 3936049666cce7bb1c6d9ffe58c7ed8f3302226a [formerly adaa5bf397387f2c566ecc935192d06cb9180de6]]]] [formerly b2c9730df7ff545d7a494408176c6be31613a25f [formerly a9edb7633ba81b86d09115911d4b6cfc202e627a] [formerly 1b7ba7097806e967986a59535f4d085140162fde [formerly 7046ff0680badd849fb92438f6498afc28f3ac39]] [formerly aa6095506df65899fd9d7bd8965f58a72f600f75 [formerly 7118548506428acd3305fc591a25791942f5d26a] [formerly 4bd543d04fb9c04d5146831ec56fb00f6ead4d39 [formerly 2c75e2312004330b794346785f1f80cd7f9334d5]]] [formerly 1b51ae927790dca324e58f4311fae7aca912eefc [formerly 7736142022053827f12eaaf8be63bc2690d5e603] [formerly d763b11b8014a3fe4f1000b5b4598efce7d95b86 [formerly 277519674d8263463cf4d947ad26fc36c128a126]] [formerly ba4b83ed5fcb60765136bf87c3c0ca4e8dbed8a9 [formerly 1a729a8550ca4f6e8830549a416fc4f0266760aa] [formerly 20b00f9b8568b15d40c3fd2a34bd96483673a1ba [formerly d302f30104a26e64dcccb2e01e15a867aa8c130f]]]]]
    Former-commit-id: 744307c77851096c6d0073b5be71b16053e06e9f [formerly dc15438fb23441ca4493b8810e89d54484808241] [formerly cadd8664a3785ca34ce7d30508d5d290eb6e509f [formerly 18fd04dbe86e68ef6c6c34433c1abcd6de6cc4e3]] [formerly a2fcf4c6b36ceb15c2744495051e61007cabc809 [formerly 12fbbc882c014e8e6ddbbaf14a64d01b53d1a13a] [formerly a51a41b9bbd5c905d3819e995705ac9dbfb68a1c [formerly 215d49d7fe576693ada18ac586c2458a4fd97588]]] [formerly e7900f6a574408f56b63b57df0b6b615c2433671 [formerly 05c64be30c6e50aa68fac3a9a61f2c02396e9ebe] [formerly b76bc758374c7fd92e5dd2ff2567c196b320907e [formerly 372b96f953f98fe3bead0744aa3bb43ecdf7b911]] [formerly fb15204a34e359dd6abe31583480637f740b7bfd [formerly 7591040ecb3d1a0ffe2309a8fd76852dfcf2c176] [formerly 54e15553eb0a0f6b55a07ac024bf1d8784e58e3d [formerly e2284c3c748b875875287d2e893a63a0d2afb976]]]]
    Former-commit-id: 8f15170f8b1d42c56f03c2ea57be57f9fb9ad408 [formerly dfbc237604a11c49782c5e0d562fa96fbf5e44ba] [formerly 8716e46afb33ae28506ab27b11aebf29b9f5f704 [formerly b01540854d4ed548fa6291cf4ab40b27792294eb]] [formerly 668d383197d7f649f9f1733ae33489306d2e2f20 [formerly b4f9e2e2a36b712c118e1f6238642ad99fd62403] [formerly 94744566da5e822e6f2e21e1ddc46f8eb66c263f [formerly f1167628b6ca14ae75efa73cd1144d93b7e66a57]]]
    Former-commit-id: 770ef3da302cb3defc19599b178710145c1a018b [formerly cf3e771d288717be6e29cb239fd049e4634f068b] [formerly b85eb1b9636b51098cf273d976babc548440eb5d [formerly 7f1351ffb7af5eeb73e8786a213406f35150cb08]]
    Former-commit-id: 49fa36b3189ebe7b769179c5078b061a05d741d7 [formerly 65b5ebb5ec18f31fec9cb4c8d2830aca5bc285ab]
    Former-commit-id: b42d3e91b95e9765a14d7b9429a1d506eda6d51b

--- build.sh
+++ build.sh
@@ -16,6 +16,18 @@ openscad -o builds/delta_stage_microscope_smart_brim.stl -D 'brim_radius = 5' -D
 openscad -o builds/base_raspi_sangaboard.stl openscad/base_raspi_sangaboard.scad
 openscad -o builds/base.stl openscad/base.scad
 openscad -o builds/feet.stl openscad/feet.scad
+openscad -o builds/actuator_assembly_tools.stl openflexure-microscope/openscad/actuator_assembly_tools.scad
+openscad -o builds/lens_tool.stl openflexure-microscope/openscad/lens_tool.scad
+openscad -o builds/sample_clips.stl openflexure-microscope/openscad/sample_clips.scad
+openscad -o builds/small_gears.stl openflexure-microscope/openscad/small_gears.scad
+openscad -o builds/large_gears.stl openflexure-microscope/openscad/gears.scad
+openscad -o builds/illumination_dovetail.stl openflexure-microscope/openscad/illumination_dovetail.scad
+openscad -o builds/picamera_2_cover.stl openflexure-microscope/openscad/cameras/picamera_2_cover.scad
+openscad -o builds/fl_cube.stl openflexure-microscope/openscad/fl_cube.scad
+openscad -o builds/condenser.stl openflexure-microscope/openscad/condenser.scad
 
 #optional parts
 openscad -o builds/35mm_petri_dish_holder.stl openscad/35mm_petri_dish_holder.scad
+
+
+
